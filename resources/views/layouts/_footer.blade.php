75f63bcb001a7754ddfb3bf9492489997463ef36
vagrant <vagrant@homestead>
2017-06-23 09:27:24 +0000

    finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+      <img class="brand-icon" src="images/th.png">
+      <a href="#">
+        信思源
+      </a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="{{route('about')}}">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
